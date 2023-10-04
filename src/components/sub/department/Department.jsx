89aa721232a069a24347ad0eea8a1a785720b444
jinkyo-09 <rlawlsry0911@gmail.com>
2023-10-04 11:16:17 +0900

    멤버데이터 출력

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -3,12 +3,13 @@ import styles from './Department.module.scss';
 import clsx from 'clsx';
 import { useEffect } from 'react';
 import { useState } from 'react';
+const path = process.env.PUBLIC_URL;
 
 export default function Department() {
 	const [Department, setDepartment] = useState([]);
 	useEffect(() => {
 		//해당 useEffect구문은 컴포넌트 마운트시 한번만 동작됨
-		fetch('/DB/department.json')
+		fetch(`${path}/DB/department.json`)
 			.then((data) => data.json())
 			.then((json) => {
 				console.log(json);
@@ -21,7 +22,11 @@ export default function Department() {
 			{Department.map((member, idx) => {
 				return (
 					<article key={idx}>
+						<div className='pic'>
+							<img src={`${path}/img/${member.pic}`} alt={member.name} />
+						</div>
 						<h2>{member.name}</h2>
+						<p>{member.position}</p>
 					</article>
 				);
 			})}
